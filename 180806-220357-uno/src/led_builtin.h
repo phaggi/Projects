5e7afadf2c4f7c5e07c09dc87dc7919b2ba871c1
phaggi <phaggi@gmail.com>
2018-08-12 00:21:49 +0300

    ifndef

--- 180806-220357-uno/src/led_builtin.h
+++ 180806-220357-uno/src/led_builtin.h
@@ -1,2 +1,4 @@
 const int8_t _led_pin ();
-#define LED_PIN _led_pin()
+#ifndef LED_PIN
+  #define LED_PIN _led_pin()
+#endif //LED_PIN
--- 180806-220357-uno/src/timer_from_launch.h
+++ 180806-220357-uno/src/timer_from_launch.h
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <timer-api.h>
-#define TIME_FROM_LAUNCH _timer_from_launch(0)
+
+#ifndef TIME_FROM_LAUNCH
+  #define TIME_FROM_LAUNCH _timer_from_launch(0)
+#endif
 
 uint32_t _timer_from_launch(bool inout);
 // void _timer_start();
-#define TIMER_START() timer_init_ISR_1Hz(TIMER_DEFAULT)
+#ifndef TIMER_START()
+  #define TIMER_START() timer_init_ISR_1Hz(TIMER_DEFAULT)
+#endif
