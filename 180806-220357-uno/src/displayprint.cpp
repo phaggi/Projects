15a128b1ec0cce40533c3da636e477027655cf54
phaggi <phaggi@gmail.com>
2018-08-12 19:08:32 +0300

    displayprint test 1

--- 180806-220357-uno/src/displayprint.cpp
+++ 180806-220357-uno/src/displayprint.cpp
@@ -1,14 +1,12 @@
-#include "main.h"
+#include "displayprint.h"
 //подготовка к работе с дисплеем
                                           // Подключаем библиотеку U8glib
-U8GLIB_ST7920_128X64_1X u8g(10);                               // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
-uint32_t                timeStart;                             // Объявляем переменную для подсчёта количества секунд прошедших с момента старта скетча
-uint8_t                 timeHours;                             // Объявляем переменную для вывода часов  прошедших с момента старта скетча
-uint8_t                 timeMinutes;                           // Объявляем переменную для вывода минут  прошедших с момента старта скетча
-uint8_t                 timeSeconds;                           // Объявляем переменную для вывода секунд прошедших с момента старта скетча
 
-void displayprint() {
+void displayprint(int8_t _sensorNumb,int16_t _baseH[], bool _testOn) 
+{
   // работа с дисплеем
+  U8GLIB_ST7920_128X64_1X u8g(10);        U8GLIB_ST7920_128X64_1X u8g(10);                               // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
+                       // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
 
   u8g.firstPage();                                           // Всё что выводится на дисплей указывается в цикле: u8g.firstPage(); do{ ... команды ... };while(u8g.nextPage());
   do {
@@ -19,31 +17,36 @@ void displayprint() {
     u8g.setRot180();
 
     u8g.setFont(u8g_font_6x10);                           // Выбираем шрифт u8g_font_6x10
-    const unsigned char l = 9;
-    sensN = 1;
-    while (sensN <= (sensorNumb)) {
-      if (testdebbug) {
-        Serial.print("nachalo pechati sens ");
-        Serial.println(sensN);
-      };
-      u8g.drawStr(100, (l * sensN), "%");
-      u8g.drawStr(3, (l * sensN), "Hum sensor");                // Выводим текст
-      u8g.setPrintPos(65, l * sensN); u8g.print(sensN);         // Выводим номер сенсора
-      u8g.drawStr(73, l * sensN, "=");                          // Выводим текст "="
-      u8g.setPrintPos(82, l * sensN); u8g.print((1023 - BASEH[sensN]) * 100 / 1023); // Выводим влажность
-      //u8g.drawStr(100, l * sensN, "t");
-      if (testdebbug) {
-        Serial.print("konec pechati sens ");
-        Serial.println(sensN);
-      };
-      sec = 1;
-      //  delay(1000 * (sec)); //пауза
-      sensN += 1;
+    // const uint8_t _STEP = 9; //
+    int8_t _sensN = 1;
+    while (_sensN <= (_sensorNumb)) 
+    {
+                          if (_DEBBUG) 
+                          {
+                            Serial.print("nachalo pechati sens ");
+                            Serial.println(_sensN);
+                          };
+      int8_t _stepline=(_STEP * _sensN);
+      if ((_stepline)<64)
+      {
+            u8g.drawStr(_HUMSENSTEXT_COLUMN, (_stepline), "Hum sensor");                // Выводим текст
+            u8g.setPrintPos(_NUMSENS_COLUMN, _stepline); u8g.print(_sensN);         // Выводим номер сенсора
+            u8g.drawStr(_EQAL_COLUMN, _stepline, "=");                          // Выводим текст "="
+            u8g.setPrintPos(_HUM_COLUMN, _stepline); u8g.print((1023 - _baseH[_sensN]) * 100 / 1023); // Выводим влажность
+            u8g.drawStr(_PERCENT_COLUMN, (_stepline), "%");// значок процента после значения влажности
+      }
+                          if (_DEBBUG) {
+                            Serial.print("konec pechati sens ");
+                            Serial.println(_sensN);
+                          };
+      const int8_t sec = 1;
+      delay(1000 * (sec)); //пауза
+      _sensN += 1;
 
     };
 
-    if (testOn == 1) {
-      u8g.drawStr(3, 50, "O");                       // Выводим текст O внизу, если нажата кнопка
+    if (_testOn == 1) {
+      u8g.drawStr(_BUTTON_PRESSED_COLUMN, _BUTTON_PRESSED_ROW, "O");                       // Выводим текст O справа, если нажата кнопка
     };
 
     //u8g.drawStr(60, 9, "Humidity");                       // Выводим текст "iarduino"        в позиции  39х9
--- 180806-220357-uno/src/displayprint.h
+++ 180806-220357-uno/src/displayprint.h
@@ -1,1 +1,12 @@
+#include <U8glib.h>
+#include <Arduino.h>
 void displayprint(void);
+#define _DEBBUG 1
+#define _STEP 9
+#define _HUMSENSTEXT_COLUMN 3
+#define _NUMSENS_COLUMN 65
+#define _EQAL_COLUMN 73
+#define _HUM_COLUMN 82
+#define _PERCENT_COLUMN 100
+#define _BUTTON_PRESSED_COLUMN 120
+#define _BUTTON_PRESSED_ROW 3
