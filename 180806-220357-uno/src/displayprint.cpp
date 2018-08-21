02add4553078a0eca74b73f9b3e19e73f734c004
phaggi <phaggi@gmail.com>
2018-08-22 00:01:26 +0300

    TIME added

--- 180806-220357-uno/src/displayprint.cpp
+++ 180806-220357-uno/src/displayprint.cpp
@@ -5,14 +5,14 @@
 void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], bool _testOn) 
 {
   // работа с дисплеем
-  U8GLIB_ST7920_128X64_1X u8g(10);                         // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
+  U8GLIB_ST7920_128X64_1X u8g(_CS);                         // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
                        // Создаём объект u8g для работы с дисплеем, указывая номер вывода CS для аппаратной шины SPI
 
   u8g.firstPage();                                         // Всё что выводится на дисплей указывается в цикле: u8g.firstPage(); do{ ... команды ... };while(u8g.nextPage());
   do 
   {
-    //u8g.setColorIndex(1);                                 // Выбираем белый цвет
-    //u8g.drawBox(0, 0, 128, 11);                           // Выводим прямоугольник с координатами левого верхнего угла 0,0 и размерами 128x11 пикселей
+    // u8g.setColorIndex(1);                                 // Выбираем белый цвет
+    // u8g.drawBox(0, 0, 128, 11);                           // Выводим прямоугольник с координатами левого верхнего угла 0,0 и размерами 128x11 пикселей
     //u8g.setColorIndex(0);                                 // Выбираем цвет фона
 
     u8g.setRot180();                                        //повернул экран, т.к. он у меня кверхногами был; можно убрать, если не нужно
@@ -26,21 +26,29 @@ void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], b
                             Serial.print("nachalo pechati sens ");
                             Serial.println(_sensN);
                           };                                // /дебаг
-      int8_t _stepline=(_STEP * _sensN);                    //задаем шаг строк на экране 
+      int8_t _stepline=(_STEP * (_sensN+1));                    //задаем шаг строк на экране 
       if ((_stepline)<64)                                   //если не вышли за границу экрана, пишем следующую строку про датчики 
+      // if (true)                                   //если не вышли за границу экрана, пишем следующую строку про датчики 
+
       {
         if(_sensor_present[_sensN])                         //проверяем наличие сенсора по базе
+                                 
         {                                                   //если сенсор есть, печатаем показания
+         if (_DEBBUG)                      // дебаг
+                          {
+                            Serial.print("sens ");Serial.print(_sensN);Serial.println(" present ");
+
+                          };                                // /дебаг
             u8g.drawStr(_HUMSENSTEXT_COLUMN, (_stepline), "Hum sensor");                // Выводим текст
             u8g.setPrintPos(_NUMSENS_COLUMN, _stepline); u8g.print(_sensN);             // Выводим номер сенсора
-            u8g.drawStr(_EQAL_COLUMN, _stepline, "=");                                  // Выводим текст "="
+            u8g.drawStr(_EQAL_COLUMN, (_stepline), "=");                                  // Выводим текст "="
             u8g.setPrintPos(_HUM_COLUMN, _stepline); u8g.print((1023 - _baseH[_sensN]) * 100 / 1023); // Выводим влажность
             u8g.drawStr(_PERCENT_COLUMN, (_stepline), "%");                             // значок процента после значения влажности
                           if (_DEBBUG)                      // дебаг
                           {
                             Serial.print("sens ");Serial.print(_sensN);Serial.print(" hum ");
                             Serial.println(_baseH[_sensN]);
-                            Serial.println(u8g.nextPage());
+//
                           };                                // /дебаг
 
         }
@@ -52,7 +60,7 @@ void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], b
                           if (_DEBBUG)                      // дебаг
                           {
                             Serial.print("sens ");Serial.print(_sensN); Serial.println(" is off ");
-                            Serial.println(u8g.nextPage());
+//
                           };                                // /дебаг
         }
 
@@ -77,7 +85,7 @@ void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], b
 
     if (_testOn == 1)                                       //проверяем, нажата ли кнопка
     {                                                         
-      u8g.drawStr(_BUTTON_PRESSED_COLUMN, _BUTTON_PRESSED_ROW, "O");            // Выводим текст O справа, если нажата кнопка
+      u8g.drawStr(_BUTTON_PRESSED_COLUMN, _BUTTON_PRESSED_ROW, "o");            // Выводим текст O справа, если нажата кнопка
                           if (_DEBBUG)                      // дебаг
                           {
                             Serial.println("knopka nazhata");
@@ -85,21 +93,19 @@ void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], b
     };
     
     // закомментированный код ниже остался от примера, и пока пусть болтается
-    //u8g.drawStr(60, 9, "Humidity");                       // Выводим текст "iarduino"        в позиции  39х9
-    //u8g.setColorIndex(1);                                 // Выбираем белый цвет
-    //u8g.drawStr(20, 62, "www.iarduino.ru");               // Выводим текст "www.iarduino.ru" в позиции  20х62
     //
-    //      u8g.setFont(u8g_font_courB24n);                         // Выбираем шрифт u8g_font_courB24n
-    //      u8g.setPrintPos(  3, 43); u8g.print(timeHours / 10);    // Выводим старший разряд часов    в позиции   3х43
-    //      u8g.setPrintPos( 20, 43); u8g.print(timeHours % 10);    // Выводим младший разряд часов    в позиции  20х43
-    //      u8g.drawStr    ( 33, 43, ":");                          // Выводим двоеточие               в позиции  33х43
-    //      u8g.setPrintPos( 46, 43); u8g.print(timeMinutes / 10);  // Выводим старший разряд минут    в позиции  46х43
-    //      u8g.setPrintPos( 63, 43); u8g.print(timeMinutes % 10);  // Выводим младший разряд минут    в позиции  63х43
-    //      u8g.drawStr    ( 76, 43, ":");                          // Выводим двоеточие               в позиции  76х43
-    //      u8g.setPrintPos( 89, 43); u8g.print(timeSeconds / 10);  // Выводим старший разряд секунд   в позиции  89х43
-    //      u8g.setPrintPos(106, 43); u8g.print(timeSeconds % 10);  // Выводим младший разряд секунд   в позиции 106х43
+         u8g.setFont(u8g_font_tpssb);                         // Выбираем шрифт 
+         u8g.setPrintPos( _TIME_COLUMN_H, _TIME_ROW); u8g.print(TIME_FROM_LAUNCH/36000);    // Выводим старший разряд часов    в позиции   3х43
+         u8g.setPrintPos( _TIME_COLUMN_H2, _TIME_ROW); u8g.print((TIME_FROM_LAUNCH/3600)%10);    // Выводим младший разряд часов    в позиции  20х43
+         u8g.drawStr    ( _TIME_COLUMN_COLON1, _TIME_ROW, ":");                          // Выводим двоеточие               в позиции  33х43
+         u8g.setPrintPos( _TIME_COLUMN_M, _TIME_ROW); u8g.print((TIME_FROM_LAUNCH%60)/10);  // Выводим старший разряд минут    в позиции  46х43
+         u8g.setPrintPos( _TIME_COLUMN_M2, _TIME_ROW); u8g.print((TIME_FROM_LAUNCH%60)%10);  // Выводим младший разряд минут    в позиции  63х43
+        //  u8g.drawStr    ( _TIME_COLUMN_COLON2, _TIME_ROW, ":");                          // Выводим двоеточие               в позиции  76х43
+        //  u8g.setPrintPos( _TIME_COLUMN_S, _TIME_ROW); u8g.print(TIME_FROM_LAUNCH / 10);  // Выводим старший разряд секунд   в позиции  89х43
+        //  u8g.setPrintPos(_TIME_COLUMN_S2, _TIME_ROW); u8g.print(TIME_FROM_LAUNCH % 10);  // Выводим младший разряд секунд   в позиции 106х43
 
   }
   while (u8g.nextPage());                                   //команда отрисовки экрана
   return;
 };
+
--- 180806-220357-uno/src/displayprint.h
+++ 180806-220357-uno/src/displayprint.h
@@ -1,13 +1,27 @@
 #include <U8glib.h>
 #include <Arduino.h>
+#include "timer_from_launch.h"
+
 void displayprint(int8_t _sensorNumb,bool _sensor_present[], int16_t _baseH[], bool _testOn);
-#define _DEBBUG 1
-#define _STEP 9
-#define _HUMSENSTEXT_COLUMN 3
-#define _NUMSENS_COLUMN 65
-#define _EQAL_COLUMN 73
-#define _HUM_COLUMN 82
-#define _PERCENT_COLUMN 100
-#define _BUTTON_PRESSED_COLUMN 120
-#define _BUTTON_PRESSED_ROW 3
-#define _SEC 0
+#define _CS                     13, 12, 10
+#define _DEBBUG                 true
+#define _STEP                   9
+#define _HUMSENSTEXT_COLUMN     3
+#define _NUMSENS_COLUMN         65
+#define _EQAL_COLUMN            73
+#define _HUM_COLUMN             82
+#define _PERCENT_COLUMN         100
+#define _BUTTON_PRESSED_COLUMN  120
+#define _BUTTON_PRESSED_ROW     7
+#define _SEC                    0
+#define _TIME_ROW               53
+#define __TIME_COLUMN_STEP      21
+#define __TIME_FONT_STEP        8
+#define _TIME_COLUMN_H          42
+#define _TIME_COLUMN_M          (_TIME_COLUMN_H+__TIME_COLUMN_STEP)
+#define _TIME_COLUMN_S          (_TIME_COLUMN_M+__TIME_COLUMN_STEP)
+#define _TIME_COLUMN_H2         (_TIME_COLUMN_H+__TIME_FONT_STEP)
+#define _TIME_COLUMN_M2         (_TIME_COLUMN_M+__TIME_FONT_STEP)
+#define _TIME_COLUMN_S2         (_TIME_COLUMN_S+__TIME_FONT_STEP)
+#define _TIME_COLUMN_COLON1     (_TIME_COLUMN_H2+__TIME_FONT_STEP)
+#define _TIME_COLUMN_COLON2     (_TIME_COLUMN_M2+__TIME_FONT_STEP)         
