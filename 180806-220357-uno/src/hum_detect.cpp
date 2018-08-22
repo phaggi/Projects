15493a48ad3baf9bfc9efdd02aba254d129f378a
phaggi <phaggi@gmail.com>
2018-08-22 20:40:13 +0300

    add primer

---
+++ 180806-220357-uno/src/hum_detect.cpp
@@ -0,0 +1,43 @@
+#include "hum_detect.h"
+int16_t hum_detect (int8_t _sensorNumb, int8_t _powerPins[], bool _sensor_present[])
+{
+bool _error = false;
+                          if (_DEBBUG)
+                          {
+                            Serial.println("nachalo port_init");
+                          };
+  for (int8_t _powerPin=0;_powerPin<_sensorNumb;_powerPin++) //перебираем порты
+  {
+                          if (_DEBBUG)
+                          {
+                            Serial.print(_powerPin);Serial.println(" port_init output");
+                          };
+    if (_sensor_present[_powerPin])
+    {
+      pinMode(_powerPins[(_powerPin)], OUTPUT);    //назначаем порт, соответствующий датчику, выходом
+        delay(100);//pausa(0.1);
+      digitalWrite((_powerPins[(_powerPin)]), LOW); //делаем порт, соответствующий датчику, LOW
+      if (digitalRead(_powerPins[(_powerPin)]))        //проверяем, стал ли порт LOW
+      { 
+        _error=true;
+        Serial.print("ERROR! power port ");Serial.print(_powerPin);Serial.println("not LOW!");
+            delay(100);//pausa(0.1);
+      }
+    }
+  }
+  return (!_error);
+}
+  // ----- конец цикла инициализации портов питания датчиков
+
+  void port_analog_init (int8_t _sensorNumb, int8_t _sensorPins[], bool _sensor_present[])
+  {
+  // void init_analog_ports(unsigned char sensorNumbInit) {
+    // начало инициализации аналоговых портов
+    
+    for (int8_t _sensorPin = 0; _sensorPin<_sensorNumb; _sensorPin++) //перебираем порты
+    {
+      if (_sensor_present[_sensorPin])                                // проверяем, есть ли датчик
+      pinMode((_sensorPins[_sensorPin]), INPUT);                      //инициализируем аналоговые входы, начиная с нулевого (порты данных датчиков)
+    };
+    // конец инициализации аналоговых портов
+  }
